b7f138c1218f131c51b706967b77259b217e8e89
Hyubs Ursua <hubertursua@zeaple.com>
2013-08-12 05:09:22 +0800

    Created test script for RouteMap

---
+++ test/lib/RouteMap.js
@@ -0,0 +1,53 @@
+module.exports = function (should) {
+	describe('RouteMap', function () {
+		var RouteMap = require('../../lib/RouteMap');
+		var validPath = __dirname + '/../dummies/controller.js';
+		var invalidPath = __dirname + '/oh/alderaan';
+
+		describe('#getFile()', function () {
+			it('should return the contents of the file if path exists', function () {
+				(new RouteMap()).getFile(validPath).should.equal(require(validPath));
+			});
+
+			it('should throw an error when file could not be retrieved', function () {
+				(function(){
+					(new RouteMap()).getFile(invalidPath);
+				}).should.throw();
+			});
+		});
+
+		describe('#isValid()', function () {
+			it('should return true if an array was passed', function () {
+				(new RouteMap()).isValid([]).should.equal(true);
+			});
+
+			it('should return false if anything other than an array was passed', function () {
+				(new RouteMap()).isValid(100).should.equal(false);
+				(new RouteMap()).isValid('').should.equal(false);
+				(new RouteMap()).isValid({}).should.equal(false);
+				(new RouteMap()).isValid(null).should.equal(false);
+			});
+		});
+
+		describe('#eval()', function () {
+			var routeFileContents = 'dancing trex';
+			var routeArray = [
+				['/beagle/bark', 'beagle#bark'],
+				['/beagle/sit', 'beagle#sit']
+			];
+
+			var routeMap = new RouteMap();
+			routeMap.getRouteFile = function () {
+				return routeFileContents;
+			};
+
+			it('should return contents from a file if a string was passed', function () {
+				routeMap.eval('llama/peaches').should.equal(routeFileContents);
+			});
+
+			it('should return back the value if an array was passed', function () {
+				routeMap.eval(routeArray).should.equal(routeArray);
+			});
+		});
+	});
+};
